a5a1da766357cf18c57d11182ba2ff2acc8edc30
jixiuf <jixiuf@gmail.com>
2014-05-05 14:31:22 +0800

    dotfile  ,git pull master mac agent fix

--- mac/LaunchAgents/git_pull_master.sh
+++ mac/LaunchAgents/git_pull_master.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # cd $@ && git checkout master && git pull
-cd ~/.emacs.d/&& git fetch origin  && git push origin master:master
+cd ~/.emacs.d/&& git pull origin master:master && git push origin master:master
 # if [ -d ~/repos/proj_golang/src/zerogame.info/thserver/ ]; then
 #     cd ~/repos/proj_golang/src/zerogame.info/thserver/&& git fetch origin 
 # fi
@@ -8,7 +8,10 @@ if [ -d ~/repos/proj_golang/src/zerogame.info/thconf/ ]; then
     cd ~/repos/proj_golang/src/zerogame.info/thconf/&& git fetch origin 
 fi
 
-cd ~/repos/proj_golang/src/zerogame.info/thtools/ && git fetch origin 
+if [ -d  ~/repos/proj_golang/src/zerogame.info/thtools/ ]; then
+    cd ~/repos/proj_golang/src/zerogame.info/thtools/ && git fetch origin 
+fi
+
 if [ -d ~/repos/godlike  ] ; then
     svn cleanup ~/repos/godlike
     svn up ~/repos/godlike
