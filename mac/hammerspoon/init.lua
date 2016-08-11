56e3cd9f288058b2660cca2562a272a855a6d474
jixiuf <jixiuf@qq.com>
2016-08-11 18:06:56 +0800

    hammerspoon toggleFullScreen

--- mac/hammerspoon/init.lua
+++ mac/hammerspoon/init.lua
@@ -33,22 +33,6 @@ end)
 
 
 
----------------------------------------------------------------
--- 指定某些app 打开后的大小布局
-local laptopScreen = hs.screen.allScreens()[1]:name()
-local windowLayout = {
-   -- 第4 5 6 个参数都是用来描述窗口大小位置的，这三个参数只能设置其中一个
-   -- 区别是第4个 是按百分比排位置如 hs.layout.left50={x=0, y=0, w=0.5, h=1}
-   -- 是第5个是按象素指定大小 （此时不考虑menu与dock的）
-   -- 是第6个是按象素指定大小 （此时考虑menu与dock的）
-   {"Safari",  nil,          laptopScreen, hs.layout.maximized,    nil, nil},
-   -- {"Safari",  nil,          laptopScreen, hs.layout.left50,    nil, nil},
-   -- {"邮件",    nil,          laptopScreen, hs.layout.right50,   nil, nil},
-   {"Emacs",  nil,     laptopScreen, hs.layout.maximized, nil, nil},
-   -- {"iTunes",  "MiniPlayer", laptopScreen, nil, nil, hs.geometry.rect(0, -48, 400, 48)},
-}
-hs.layout.apply(windowLayout)
----------------------------------------------------------------
 
 
 ---------------------------------------------------------------
@@ -82,15 +66,15 @@ hs.hotkey.bind({"cmd","alt","shift"}, "D", mouseHighlight)
 ---占左半屏------------------------------------------------------------
 function moveWinLeft()
       local win = hs.window.focusedWindow()
-      local f = win:frame()
+      local curFrame = win:frame()
       local screen = win:screen()
       local max = screen:frame()
 
-      f.x = max.x
-      f.y = max.y
-      f.w = max.w / 2
-      f.h = max.h
-      win:setFrame(f)     
+      curFrame.x = max.x
+      curFrame.y = max.y
+      curFrame.w = max.w / 2
+      curFrame.h = max.h
+      win:setFrame(curFrame)     
 end
 -- hs.hotkey.bind({"cmd"}, "LEFT", function()
 --       moveWinLeft()
@@ -102,15 +86,15 @@ hs.urlevent.bind("moveWinLeft", function(eventName, params) moveWinLeft() end)
 ---cmd right 占右半屏------------------------------------------------------------
 function moveWinRight()
       local win = hs.window.focusedWindow()
-      local f = win:frame()
+      local curFrame = win:frame()
       local screen = win:screen()
       local max = screen:frame()
 
-      f.x = max.x +max.w/2
-      f.y = max.y
-      f.w = max.w / 2
-      f.h = max.h
-      win:setFrame(f)     
+      curFrame.x = max.x +max.w/2
+      curFrame.y = max.y
+      curFrame.w = max.w / 2
+      curFrame.h = max.h
+      win:setFrame(curFrame)     
 end
 -- hs.hotkey.bind({"cmd"}, "RIGHT", function()moveWinRight() end)
 hs.urlevent.bind("moveWinRight", function(eventName, params) moveWinRight() end)
@@ -118,15 +102,15 @@ hs.urlevent.bind("moveWinRight", function(eventName, params) moveWinRight() end)
 ---cmd up 占上半屏------------------------------------------------------------
 function moveWinUp()
       local win = hs.window.focusedWindow()
-      local f = win:frame()
+      local curFrame = win:frame()
       local screen = win:screen()
       local max = screen:frame()
 
-      f.x = max.x
-      f.y = max.y
-      f.w = max.w
-      f.h = max.h/2
-      win:setFrame(f)     
+      curFrame.x = max.x
+      curFrame.y = max.y
+      curFrame.w = max.w
+      curFrame.h = max.h/2
+      win:setFrame(curFrame)     
 end
 -- hs.hotkey.bind({"cmd"}, "UP", function() moveWinUp() end)
 hs.urlevent.bind("moveWinUp", function(eventName, params) moveWinUp() end)
@@ -134,15 +118,79 @@ hs.urlevent.bind("moveWinUp", function(eventName, params) moveWinUp() end)
 ---cmd down 占下半屏------------------------------------------------------------
 function moveWinDown()
       local win = hs.window.focusedWindow()
-      local f = win:frame()
+      local curFrame = win:frame()
       local screen = win:screen()
       local max = screen:frame()
 
-      f.x = max.x
-      f.y = max.y+max.h/2
-      f.w = max.w
-      f.h = max.h/2
-      win:setFrame(f)     
+      curFrame.x = max.x
+      curFrame.y = max.y+max.h/2
+      curFrame.w = max.w
+      curFrame.h = max.h/2
+      win:setFrame(curFrame)     
 end
 hs.urlevent.bind("moveWinDown", function(eventName, params) moveWinDown() end)
 -- hs.hotkey.bind({"cmd"}, "DOWN", function() moveWinDown() end)
+
+
+
+---------------------------------------------------------------
+-- 指定某些app 打开后的大小布局
+local laptopScreen = hs.screen.allScreens()[1]:name()
+local windowLayout = {
+   -- 第4 5 6 个参数都是用来描述窗口大小位置的，这三个参数只能设置其中一个
+   -- 区别是第4个 是按百分比排位置如 hs.layout.left50={x=0, y=0, w=0.5, h=1}
+   -- 是第5个是按象素指定大小 （此时不考虑menu与dock的）
+   -- 是第6个是按象素指定大小 （此时考虑menu与dock的）
+   {"Safari",  nil,          laptopScreen, hs.layout.maximized,   nil , nil},
+   -- {"Safari",  nil,          laptopScreen, hs.layout.left50,    nil, nil},
+   -- {"邮件",    nil,          laptopScreen, hs.layout.right50,   nil, nil},
+   {"Emacs",  nil,     laptopScreen, hs.layout.maximized, nil, nil},
+   -- {"Emacs",  nil,     laptopScreen, nil, nil, hs.geometry.rect(100, 100, 800, 480)},
+   -- {"iTunes",  "MiniPlayer", laptopScreen, nil, nil, hs.geometry.rect(0, -48, 400, 48)},
+}
+hs.layout.apply(windowLayout)
+---------------------------------------------------------------
+-- -- 如果 windowLayout里有配相应app的初始大小，则将对应win设置成此大小
+-- function moveToInitPos (win,appName)
+--    for k,v in pairs(windowLayout) do
+--       if v[1]==appName then
+--          hs.alert.show(appName)
+--          if v[4] ~= nil then
+--             win:moveToUnit(v[4])
+--          elseif v[5] ~= nil then
+--             win:setFrame(v[5])
+--          elseif v[6] ~= nil then
+--             win:setFrame(v[6])
+--          end
+--          return true
+--       end
+--    end
+--    return false
+-- end
+
+
+local toggleMaximizedMap={}
+math.randomseed(os.time()) 
+-- hs.geometry.rect(0, -48, 400, 48)
+function toggleMaximized()
+   local win = hs.window.focusedWindow()
+   local app = win:application()
+   local winKey=app:bundleID() .. tostring(win:id())
+   local curFrame = win:frame()
+   local originFrame=toggleMaximizedMap[winKey]
+   local screen = win:screen()
+   local max = screen:frame()
+   -- hs.window.setFrameCorrectness=true
+   win:maximize(0)           -- 0s duration (无动画马上最大化)
+   local maximizedFrame= win:frame() -- 最大化后的尺寸
+   if math.abs(maximizedFrame.w-curFrame.w)<10 and math.abs(maximizedFrame.h-curFrame.h)<10 then -- 只要窗口大小跟全屏后的尺寸相差不大就认为是全屏状态
+      if  originFrame then
+         win:setFrame(originFrame,0) -- 恢复成初始窗口大小
+      else                      -- 没有存窗口的初始大小，则随机将其调到一个位置
+         win:moveToUnit(hs.geometry.rect(math.random()*0.1,math.random()*0.1, 0.718, 0.718),0.1)
+      end
+   else                         -- 当前是非最大化状态，
+      toggleMaximizedMap[winKey]=hs.geometry.copy(curFrame) -- 存储当前窗口大小
+   end
+end
+hs.hotkey.bind({"cmd"}, "M", toggleMaximized)
