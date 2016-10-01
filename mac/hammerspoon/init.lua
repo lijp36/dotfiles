7b01da4e44b326450fbbae42efdcb9de54f7c03d
jixiuf <jixiuf@qq.com>
2016-10-01 22:58:47 +0800

    squirrel_toggle_option

--- mac/hammerspoon/init.lua
+++ mac/hammerspoon/init.lua
@@ -813,6 +813,10 @@ function emacs_evil_insert_state()
    if app:bundleID()=="org.gnu.Emacs" then
       -- emacs 中f18 绑定为进入evil-insert-state
       -- 此处模拟在emacs 中按f18键
+
+      -- (global-set-key (kbd "<f18>") 'evil-insert-state) ;mac karabiner用来控制输入法
+      -- (define-key isearch-mode-map (kbd "<f18>") 'evil-insert-state) ;详见isearch-pre-command-hook
+
       hs.eventtap.keyStroke({}, "f18")
    elseif app:bundleID()=="com.googlecode.iterm2" then -- 如果是在终端下，通过终端的窗口标题来区分当前是否在用emacs编辑文件
       -- 如果是在iterm2终端中，也模拟按下f18键，
@@ -829,4 +833,5 @@ function emacs_evil_insert_state()
    end
 end
 hs.urlevent.bind("emacs_evil_insert_state", function() emacs_evil_insert_state() end)
-
+-- open -g "hammerspoon://hello"
+-- hs.urlevent.bind("hello", function() hs.eventtap.keyStroke({"shift"}, "capslock") end)
