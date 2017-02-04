7f15b9486160cbca012d8ce6507dd172132a81cd
jixiuf <jixiuf@qq.com>
2017-02-04 16:50:24 +0800

    init

--- mac/hammerspoon/init.lua
+++ mac/hammerspoon/init.lua
@@ -21,9 +21,13 @@ local hyper={"cmd"}
 function reloadConfig(files)
    doReload = false
    for _,file in pairs(files) do
-      if file:sub(-4) == ".lua" then
+      if file == "init.lua" then
          doReload = true
       end
+
+      -- if file:sub(-4) == ".lua" then
+      --    doReload = true
+      -- end
    end
    if doReload then
       hs.reload()
