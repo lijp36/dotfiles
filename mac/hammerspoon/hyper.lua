54306e9a7da63109536645b88a393032e5e8569a
jixiuf <jixiuf@qq.com>
2017-07-14 17:40:53 +0800

    fn-v paste

--- mac/hammerspoon/hyper.lua
+++ mac/hammerspoon/hyper.lua
@@ -3,3 +3,5 @@ hyper={"cmd","ctrl","alt","shift"}
 hyper2={"cmd","ctrl","alt"}
 
 hs.hotkey.bind(hyper2, "v", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end )
+hs.urlevent.bind("fnv_paste", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
+
