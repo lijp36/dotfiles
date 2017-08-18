2bbfe47b0186da71ab735f4ab66a99d7dfbf7303
jixiuf <jixiuf@gmail.com>
2017-08-18 12:24:25 +0800

    backup cmderconfig

--- w32/make.bat
+++ w32/make.bat
@@ -1,6 +1,12 @@
 ;= @echo off
 rem this is comment
 xcopy /y .gitconfig %USERPROFILE%\
+
+del c:\cmder\config /s /q /f
+rd /s /q c:\cmder\config
+md c:\cmder
+xcopy /y /e cmder_config c:\cmder\config\
+
 noAdriver.reg
 copyto.reg
 user-env.reg
