75f156e77073556c0f44178a7bf495be97a35dcf
jixiuf <jixiuf@qq.com>
2017-08-29 16:27:26 +0800

    update make.bat

--- w32/make.bat
+++ w32/make.bat
@@ -6,8 +6,8 @@ del c:\cmder\config /s /q /f
 rd /s /q c:\cmder\config
 md c:\cmder
 rem  /j means directory link
-mklink /j  c:\cmder\config cmder_config     
-mklink  %USERPROFILE%\.bashrc .bashrc    
+mklink /j  c:\cmder\config %cd%\cmder_config     
+mklink  %USERPROFILE%\.bashrc %cd%\.bashrc    
 rem  xcopy /y /e c:\cmder\config\ cmder_config\ 
 
 noAdriver.reg
