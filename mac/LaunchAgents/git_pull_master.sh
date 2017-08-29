280013fde75f9780aa231ffd2718619fb8e589dd
jixiuf <jixiuf@qq.com>
2017-08-29 22:31:06 +0800

    auto fetch emacs

--- mac/LaunchAgents/git_pull_master.sh
+++ mac/LaunchAgents/git_pull_master.sh
@@ -12,25 +12,28 @@ fi
 if [ -d ~/repos/dotfiles/ ]; then
     cd ~/repos/dotfiles/  &&git pull --commit &&git push
 fi
+if [ -d ~/repos/emacs/ ]; then
+    cd ~/repos/emacs/  &&git fetch
+fi
 
-if [ -d $GOPATH/src/najaplus.com/gamebase/ ]; then
-    cd $GOPATH/src/najaplus.com/gamebase/  &&git fetch && git push ; make get-deps;make
-fi
-if [ -d $GOPATH/src/najaplus.com/zjh/ ]; then
-    cd $GOPATH/src/najaplus.com/zjh/  &&git fetch && git push&&make
-fi
-if [ -d ~/svn/ ]; then
-    cd ~/svn/;svn cleanup ;svn up
-fi
-if [ -d ~/repos/svn/game1git ]; then
-    cd ~/repos/svn/game1git  &&git stash && git svn fetch&&git svn rebase &&git stash pop
-fi
-if [ -d ~/repos/svn/game2git ]; then
-    cd ~/repos/svn/game2git  &&git stash && git svn fetch&&git svn rebase &&git stash pop
-fi
-if [ -d ~/repos/svn/todo ]; then
-    cd ~/repos/svn/todo  &&git stash && git svn fetch&&git svn rebase &&git stash pop
-fi
+# if [ -d $GOPATH/src/najaplus.com/gamebase/ ]; then
+#     cd $GOPATH/src/najaplus.com/gamebase/  &&git fetch && git push ; make get-deps;make
+# fi
+# if [ -d $GOPATH/src/najaplus.com/zjh/ ]; then
+#     cd $GOPATH/src/najaplus.com/zjh/  &&git fetch && git push&&make
+# fi
+# if [ -d ~/svn/ ]; then
+#     cd ~/svn/;svn cleanup ;svn up
+# fi
+# if [ -d ~/repos/svn/game1git ]; then
+#     cd ~/repos/svn/game1git  &&git stash && git svn fetch&&git svn rebase &&git stash pop
+# fi
+# if [ -d ~/repos/svn/game2git ]; then
+#     cd ~/repos/svn/game2git  &&git stash && git svn fetch&&git svn rebase &&git stash pop
+# fi
+# if [ -d ~/repos/svn/todo ]; then
+#     cd ~/repos/svn/todo  &&git stash && git svn fetch&&git svn rebase &&git stash pop
+# fi
 
 
 
