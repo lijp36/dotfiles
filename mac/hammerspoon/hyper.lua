5535db92d9bb24cc9446ba1af669583bcea4074b
jixiufeng <jixiufeng@luojilab.com>
2018-08-09 21:09:43 +0800

    paste_dwim

--- mac/hammerspoon/hyper.lua
+++ mac/hammerspoon/hyper.lua
@@ -2,8 +2,18 @@
 hyper={"cmd","ctrl","alt","shift"}
 hyper2={"cmd","ctrl","alt"}
 
-hs.hotkey.bind(hyper2, "v", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end )
-hs.urlevent.bind("fnv_paste", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
+local function paste_dwim()
+   local app =hs.application.frontmostApplication()
+   if app ~= nil and app:bundleID() == "com.cisco.Cisco-AnyConnect-Secure-Mobility-Client"    then
+      hs.eventtap.keyStrokes("hello")
+
+   else
+      hs.eventtap.keyStrokes(hs.pasteboard.getContents())
+   end
+end
+
+hs.hotkey.bind(hyper2, "v", paste_dwim)
+hs.urlevent.bind("fnv_paste", paste_dwim)
 
 
 hs.hotkey.bind(hyper, "f12", function() hs.caffeinate.lockScreen() end)
