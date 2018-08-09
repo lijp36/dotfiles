900da6c50c6f105ff65ede3a8ca3042b21db5aad
jixiufeng <jixiufeng@luojilab.com>
2018-08-09 22:16:05 +0800

    paste

--- mac/hammerspoon/hyper.lua
+++ mac/hammerspoon/hyper.lua
@@ -1,18 +1,17 @@
 -- other lua require this
 hyper={"cmd","ctrl","alt","shift"}
-hyper2={"cmd","ctrl","alt"}
+hyper2={"cmd","ctrl"}
 
 local function paste_dwim()
    local app =hs.application.frontmostApplication()
    if app ~= nil and app:bundleID() == "com.cisco.Cisco-AnyConnect-Secure-Mobility-Client"    then
-      hs.eventtap.keyStrokes("hello")
-
+      hs.eventtap.keyStrokes("tZRg49y14K")
    else
       hs.eventtap.keyStrokes(hs.pasteboard.getContents())
    end
 end
 
-hs.hotkey.bind(hyper2, "v", paste_dwim)
+-- hs.hotkey.bind(hyper2, "v", paste_dwim)
 hs.urlevent.bind("fnv_paste", paste_dwim)
 
 
