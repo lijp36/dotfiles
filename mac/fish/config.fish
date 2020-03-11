86e210965d6e27ad21968a0363a447f0a1f78479
jixiuf <jixiuf@qq.com>
2020-03-11 22:46:51 +0800

    fish remove unused vterm config

--- mac/fish/config.fish
+++ mac/fish/config.fish
@@ -10,22 +10,9 @@ function vterm_print;
         printf "\e]%s\e\\" "$argv"
     end
 end
-    
-    function vterm_cmd_end --on-event fish_preexec; printf "\e]51;B\e\\"; end
-         
-function vterm_prompt_begin;
-    vterm_print '51;C'
-end
 
 function vterm_prompt_end;
     vterm_print '51;A'(whoami)'@'(hostname)':'(pwd)
-end
-
-
-functions -c fish_mode_prompt vterm_old_fish_mode_prompt
-function fish_mode_prompt --description 'Write out the prompt; do not replace this. Instead, put this at end of your file.'
-    vterm_prompt_begin
-    vterm_old_fish_mode_prompt
 end
 
 functions -c fish_prompt vterm_old_fish_prompt
@@ -34,6 +21,5 @@ function fish_prompt --description 'Write out the prompt; do not replace this. I
     # using the string builtin from fish, but to make sure any escape codes
     # are correctly interpreted, use %b for printf.
     printf "%b" (string join "\n" (vterm_old_fish_prompt))
-
     vterm_prompt_end
 end
